ec077915e8a2aa5402fd160de5e78e8b84fcb7b6
ThomasGrelet <thomas.grelet@univ-tlse3.fr>
2026-03-11 23:36:13 +0100

    interface IAffichage et déclaration des methodes dans Affichage

--- src/affichage/Affichage.java
+++ src/affichage/Affichage.java
@@ -1,5 +1,55 @@
 package affichage;
 
+import jeu.Couleur;
+
 public class Affichage implements IAffichage {
+
+	@Override
+	public void afficherJeu(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherJoueur(String nomJoueur, int vie, Couleur couleurPion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherCaseNormale(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherCaseRouletteRusse(String message, int newPosition) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherCaseVolVie(String message, String nomJoueur1, String nomJoueur2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherResultatDes(int resultatDes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherDebutPartie(String nomJoueur1, String nomJoueur2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afficherFinPartie(String message) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/affichage/IAffichage.java
+++ src/affichage/IAffichage.java
@@ -1,5 +1,16 @@
 package affichage;
 
+import jeu.Couleur;
+
 public interface IAffichage {
+	
+	void afficherJeu(String message);
+	void afficherJoueur(String nomJoueur, int vie, Couleur couleurPion);
+	void afficherCaseNormale(String message);
+	void afficherCaseRouletteRusse(String message, int newPosition);
+	void afficherCaseVolVie(String message, String nomJoueur1, String nomJoueur2);
+	void afficherResultatDes(int resultatDes);
+	void afficherDebutPartie(String nomJoueur1, String nomJoueur2);
+	void afficherFinPartie(String message);
 
 }
